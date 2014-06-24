bcc44c1d9dc34fe26fd675e353c1f173364e83f8
tanla <tanla@Abhis-Mac-mini.local>
2014-06-24 16:37:28 +0530

    Initial Commit

---
+++ FbTwitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FbTwitter
+//
+//  Created by tanla on 24/06/14.
+//  Copyright (c) 2014 tanla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FbTwitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FbTwitter
+//
+//  Created by tanla on 24/06/14.
+//  Copyright (c) 2014 tanla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
